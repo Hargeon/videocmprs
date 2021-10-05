49398e6b4b53005743bd48ac2bccf707ae3c444e
Hargeon <hirami14590@gmail.com>
2021-10-05 11:46:13 +0300

    Add primary key to users migration

--- db/migrations/common/20211001004401_users.sql
+++ db/migrations/common/20211001004401_users.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users (
-    id bigserial not null unique,
+    id bigserial not null unique PRIMARY KEY,
     email varchar(255) not null unique,
     password_hash varchar(255) not null,
     created_at  timestamp not null
