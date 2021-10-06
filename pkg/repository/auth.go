bde3e863291e7903f3eee58f51cc1c438267c554
Hargeon <hirami14590@gmail.com>
2021-10-06 15:57:21 +0300

    Add context for AuthRepository GetUser

--- pkg/repository/auth.go
+++ pkg/repository/auth.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+const timeOutQuery = 5 * time.Second
+
 // AuthRepository represent user manipulation
 type AuthRepository struct {
 	db *sqlx.DB
@@ -22,7 +24,7 @@ func NewAuthRepository(db *sqlx.DB) *AuthRepository {
 func (auth AuthRepository) CreateUser(user *model.User) (int64, error) {
 	var id int64
 	query := fmt.Sprintf("INSERT INTO %s (email, password_hash, created_at) VALUES ($1, $2, $3) RETURNING id", model.UserTableName)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), timeOutQuery)
 	defer cancel()
 
 	row := auth.db.QueryRowxContext(ctx, query, user.Email, user.Password, user.CreatedAt)
@@ -34,6 +36,9 @@ func (auth AuthRepository) CreateUser(user *model.User) (int64, error) {
 func (auth AuthRepository) GetUser(email, password string) (int64, error) {
 	var id int64
 	query := fmt.Sprintf("SELECT id FROM %s WHERE email = $1 AND password_hash = $2 LIMIT 1", model.UserTableName)
-	err := auth.db.QueryRowx(query, email, password).Scan(&id)
+	ctx, cancel := context.WithTimeout(context.Background(), timeOutQuery)
+	defer cancel()
+
+	err := auth.db.QueryRowxContext(ctx, query, email, password).Scan(&id)
 	return id, err
 }
