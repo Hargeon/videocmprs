66d0da10305764447dc640e316412ae61c13163e
Hargeon <hirami14590@gmail.com>
2021-10-13 22:56:28 +0300

    Add api handler

---
+++ api/api.go
@@ -0,0 +1,30 @@
+// Package api uses for routing
+package api
+
+import (
+	"github.com/Hargeon/videocmprs/api/middleware"
+	"github.com/Hargeon/videocmprs/api/user"
+	"github.com/gofiber/fiber/v2"
+	"github.com/jmoiron/sqlx"
+)
+
+type Handler struct {
+	db *sqlx.DB
+}
+
+// NewHandler returns new Handler
+func NewHandler(db *sqlx.DB) *Handler {
+	return &Handler{db: db}
+}
+
+// InitRoutes initializes and returns *fiber.App
+func (h *Handler) InitRoutes() *fiber.App {
+	app := fiber.New()
+	api := app.Group("/api")
+
+	v1 := api.Group("/v1")
+	v1.Use(middleware.AcceptHeader)
+	v1.Mount("/users", user.NewHandler(h.db).InitRoutes())
+
+	return app
+}
