734b18d20e3fdab73edfe7197a4ebf10bfd0b4f6
Hargeon <hirami14590@gmail.com>
2021-10-13 22:55:56 +0300

    Add Accept header middleware

---
+++ api/middleware/accept.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/jsonapi"
+	"net/http"
+)
+
+const headerAccept = "Accept"
+
+func AcceptHeader(c *fiber.Ctx) error {
+	if string(c.Request().Header.Peek(headerAccept)) != jsonapi.MediaType {
+		return c.Status(http.StatusUnsupportedMediaType).SendString("Unsupported Media Type")
+	}
+	return c.Next()
+}
