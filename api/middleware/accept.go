8515318fbf768e0f49b97bbd2023c8634879e4c7
Hargeon <hirami14590@gmail.com>
2021-10-14 21:40:15 +0300

    Add comment to AcceptHeader

--- api/middleware/accept.go
+++ api/middleware/accept.go
@@ -8,6 +8,7 @@ import (
 
 const headerAccept = "Accept"
 
+// AcceptHeader validate Accept header
 func AcceptHeader(c *fiber.Ctx) error {
 	if string(c.Request().Header.Peek(headerAccept)) != jsonapi.MediaType {
 		return c.Status(http.StatusUnsupportedMediaType).SendString("Unsupported Media Type")
