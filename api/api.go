b482e17a311af71ee421abd6fcfed3a01328753a
Hargeon <hirami14590@gmail.com>
2021-10-18 18:26:55 +0300

    Add auth handler

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@
 package api
 
 import (
+	"github.com/Hargeon/videocmprs/api/auth"
 	"github.com/Hargeon/videocmprs/api/middleware"
 	"github.com/Hargeon/videocmprs/api/user"
 	"github.com/gofiber/fiber/v2"
@@ -25,6 +26,7 @@ func (h *Handler) InitRoutes() *fiber.App {
 	v1 := api.Group("/v1")
 	v1.Use(middleware.AcceptHeader)
 	v1.Mount("/users", user.NewHandler(h.db).InitRoutes())
+	v1.Mount("/auth", auth.NewHandler(h.db).InitRoutes())
 
 	return app
 }
---
+++ api/auth/handler.go
@@ -0,0 +1,59 @@
+package auth
+
+import (
+	"bytes"
+	"github.com/Hargeon/videocmprs/api/response"
+	"github.com/Hargeon/videocmprs/pkg/repository/auth"
+	"github.com/Hargeon/videocmprs/pkg/repository/user"
+	"github.com/Hargeon/videocmprs/pkg/service"
+	authsrv "github.com/Hargeon/videocmprs/pkg/service/auth"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/jsonapi"
+	"github.com/jmoiron/sqlx"
+	"net/http"
+)
+
+type Handler struct {
+	srv service.TokenAble
+}
+
+func NewHandler(db *sqlx.DB) *Handler {
+	repo := auth.NewRepository(db)
+	srv := authsrv.NewService(repo)
+	return &Handler{srv: srv}
+}
+
+func (h *Handler) InitRoutes() *fiber.App {
+	router := fiber.New()
+	router.Post("/sign-in", h.signIn)
+	return router
+}
+
+func (h *Handler) signIn(c *fiber.Ctx) error {
+	usr := new(user.Resource)
+	bodyReader := bytes.NewReader(c.Body())
+	if err := jsonapi.UnmarshalPayload(bodyReader, usr); err != nil {
+		errors := []string{err.Error()}
+		return response.ErrorJsonApiResponse(c, http.StatusBadRequest, errors)
+	}
+
+	validation := validator.New()
+	if err := validation.StructPartial(usr, "Email", "Password"); err != nil {
+		errors := []string{"Validation failed"}
+		return response.ErrorJsonApiResponse(c, http.StatusBadRequest, errors)
+	}
+
+	linkable, err := h.srv.GenerateToken(c.Context(), usr)
+	if err != nil {
+		errors := []string{err.Error()}
+		return response.ErrorJsonApiResponse(c, http.StatusInternalServerError, errors)
+	}
+
+	payload, err := jsonapi.Marshal(linkable)
+	if err != nil {
+		errors := []string{err.Error()}
+		return response.ErrorJsonApiResponse(c, http.StatusInternalServerError, errors)
+	}
+	return c.Status(http.StatusCreated).JSON(payload)
+}
