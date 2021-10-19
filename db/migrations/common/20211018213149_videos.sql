a2a8a1636516a5d5e5fb9091476ee07cdb56cfda
Hargeon <hirami14590@gmail.com>
2021-10-19 21:44:05 +0300

    Change videos migration

--- db/migrations/common/20211018213149_videos.sql
+++ db/migrations/common/20211018213149_videos.sql
@@ -1,7 +1,5 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
 CREATE TABLE IF NOT EXISTS videos (
     id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -10,7 +8,9 @@ CREATE TABLE IF NOT EXISTS videos (
     resolution varchar(255),
     service_id varchar(255),
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+    );
+-- +goose StatementEnd
+
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS videos
