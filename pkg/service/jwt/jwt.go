b7ac5deaa994f4ea701bbf354b1a989ce693fa3f
Hargeon <hirami14590@gmail.com>
2021-10-19 17:36:52 +0300

    Add parsing token to jwt

--- pkg/service/jwt/jwt.go
+++ pkg/service/jwt/jwt.go
@@ -2,6 +2,7 @@
 package jwt
 
 import (
+	"errors"
 	"github.com/dgrijalva/jwt-go"
 	"os"
 	"time"
@@ -25,4 +26,21 @@ func SignedString(id int64) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
+}
+
+func ParseToken(tokenStr string) (int64, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &authClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return []byte(os.Getenv("TOKEN_SECRET")), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	if claims, ok := token.Claims.(*authClaims); ok && token.Valid {
+		return claims.Id, nil
+	}
+	return 0, errors.New("invalid token")
 }
