ca6ce8e542463fccd23fe160cdd3e7a4d8c2c52d
Hargeon <hirami14590@gmail.com>
2021-10-20 18:19:53 +0300

    Add requests migration

--- db/migrations/common/20211019214146_requests.sql
+++ db/migrations/common/20211019214146_requests.sql
@@ -1,17 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS videos (
+CREATE TABLE IF NOT EXISTS requests (
     id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    size BIGINT,
-    bitrate BIGINT,
-    resolution varchar(255),
-    service_id varchar(255),
+    user_id BIGINT REFERENCES users,
+    status VARCHAR(255),
+    details VARCHAR(255),
+    original_file_id BIGINT,
+    converted_file_id BIGINT,
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS videos
+DROP TABLE IF EXISTS requests;
 -- +goose StatementEnd
