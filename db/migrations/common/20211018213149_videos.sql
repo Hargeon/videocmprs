6e61dafbe1fd38066a3e38685e6f14c3b72435c4
Hargeon <hirami14590@gmail.com>
2021-10-25 20:14:24 +0300

    Update video migration

--- db/migrations/common/20211018213149_videos.sql
+++ db/migrations/common/20211018213149_videos.sql
@@ -3,10 +3,10 @@
 CREATE TABLE IF NOT EXISTS videos (
     id BIGSERIAL NOT NULL UNIQUE PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    size BIGINT,
+    size BIGINT NOT NULL,
     bitrate BIGINT,
     resolution varchar(255),
-    ratio varchar(255)
+    ratio varchar(255),
     service_id varchar(255),
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
