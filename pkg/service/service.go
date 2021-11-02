9a37a1492fe6030e3cf9f78f22367c0bc62b478a
Hargeon <hirami14590@gmail.com>
2021-11-03 00:53:02 +0300

    Add paginator

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -3,8 +3,11 @@ package service
 
 import (
 	"context"
-	"github.com/google/jsonapi"
 	"mime/multipart"
+
+	"github.com/Hargeon/videocmprs/api/query"
+
+	"github.com/google/jsonapi"
 )
 
 type Creator interface {
@@ -15,15 +18,20 @@ type Retriever interface {
 	Retrieve(ctx context.Context, resource jsonapi.Linkable) (jsonapi.Linkable, error)
 }
 
-type Tokenable interface {
-	GenerateToken(ctx context.Context, resource jsonapi.Linkable) (jsonapi.Linkable, error)
+type Paginator interface {
+	List(ctx context.Context, params query.Params) ([]jsonapi.Linkable, error)
 }
 
-type UserService interface {
-	Creator
-	Retriever
+type Tokenable interface {
+	GenerateToken(ctx context.Context, resource jsonapi.Linkable) (jsonapi.Linkable, error)
+	Retrieve(ctx context.Context, id int64) (jsonapi.Linkable, error)
 }
 
 type CloudStorage interface {
 	Upload(ctx context.Context, header *multipart.FileHeader) (string, error)
+}
+
+type Request interface {
+	Creator
+	Paginator
 }
