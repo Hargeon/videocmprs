738e4dcb6ecafa713c7c9acb253112f8a13cd92f
Hargeon <hirami14590@gmail.com>
2021-11-03 00:54:24 +0300

    Add retieve for auth handler

--- api/auth/handler.go
+++ api/auth/handler.go
@@ -1,16 +1,20 @@
+// Package auth uses for authorization
 package auth
 
 import (
 	"bytes"
-	"github.com/Hargeon/videocmprs/api/response"
-	"github.com/Hargeon/videocmprs/pkg/repository/user"
-	"github.com/Hargeon/videocmprs/pkg/service"
-	"github.com/Hargeon/videocmprs/pkg/service/auth"
+	"net/http"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/jsonapi"
 	"github.com/jmoiron/sqlx"
-	"net/http"
+
+	"github.com/Hargeon/videocmprs/api/middleware"
+	"github.com/Hargeon/videocmprs/api/response"
+	"github.com/Hargeon/videocmprs/pkg/repository/user"
+	"github.com/Hargeon/videocmprs/pkg/service"
+	"github.com/Hargeon/videocmprs/pkg/service/auth"
 )
 
 type Handler struct {
@@ -26,6 +30,8 @@ func NewHandler(db *sqlx.DB) *Handler {
 func (h *Handler) InitRoutes() *fiber.App {
 	router := fiber.New()
 	router.Post("/sign-in", h.signIn)
+	router.Use(middleware.UserIdentify)
+	router.Get("/me", h.retrieve)
 	return router
 }
 
@@ -57,6 +63,13 @@ func (h *Handler) signIn(c *fiber.Ctx) error {
 	return nil
 }
 
-func (h *Handler) me(c *fiber.Ctx) error {
-	return nil
+// retrieve return user params
+func (h *Handler) retrieve(c *fiber.Ctx) error {
+	id := c.Locals("user_id").(int64)
+	res, err := h.srv.Retrieve(c.Context(), id)
+	if err != nil {
+		errors := []string{err.Error()}
+		return response.ErrorJsonApiResponse(c, http.StatusInternalServerError, errors)
+	}
+	return jsonapi.MarshalPayload(c.Status(http.StatusOK), res)
 }
