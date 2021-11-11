c088c4bcdfe0afca60c2a517af9f390abbfec4fe
Hargeon <hirami14590@gmail.com>
2021-11-11 14:30:55 +0300

    Add tests for auth retrieve route

--- pkg/service/auth/authorization_test.go
+++ pkg/service/auth/authorization_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
-func TestExists(t *testing.T) {
+func TestGenerateToken(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("Unexpected error when opening a stub db connection, error: %s\n", err)
@@ -90,6 +90,80 @@ func TestExists(t *testing.T) {
 	}
 }
 
-func TestMe(t *testing.T) {
+func TestRetrieve(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Unexpected error when opening a stub db connection, error: %s\n", err)
+	}
 
+	cases := []struct {
+		name          string
+		id            int64
+		mock          func()
+		expectedID    int64
+		expectedEmail string
+		errorPresent  bool
+	}{
+		{
+			name: "Should find user",
+			id:   1,
+			mock: func() {
+				mock.ExpectQuery(fmt.Sprintf("SELECT id, email FROM %s", user.TableName)).
+					WithArgs(1).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "email"}).AddRow("1", "check@check.com"))
+			},
+			expectedID:    1,
+			expectedEmail: "check@check.com",
+			errorPresent:  false,
+		},
+
+		{
+			name: "Should not find user",
+			id:   1,
+			mock: func() {
+				mock.ExpectQuery(fmt.Sprintf("SELECT id, email FROM %s", user.TableName)).
+					WithArgs(1).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "email"}))
+			},
+			errorPresent: true,
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mock()
+			repo := user.NewRepository(db)
+			srv := NewService(repo)
+			usrLinkable, err := srv.Retrieve(context.Background(), testCase.id)
+
+			if err != nil && !testCase.errorPresent {
+				t.Errorf("Unexpected error: %s\n", err.Error())
+			}
+
+			if err == nil && testCase.errorPresent {
+				t.Errorf("Should be error\n")
+			}
+
+			if err == nil {
+				usr, ok := usrLinkable.(*user.Resource)
+				if !ok {
+					t.Fatalf("Invalid type assertion *user.Resource\n")
+				}
+
+				if usr.ID != testCase.expectedID {
+					t.Errorf("Invalid id, expected: %d, got: %d\n",
+						testCase.expectedID, usr.ID)
+				}
+
+				if usr.Email != testCase.expectedEmail {
+					t.Errorf("Invalid email, expected: %s, got: %s\n",
+						testCase.expectedEmail, usr.Email)
+				}
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s\n", err)
+			}
+		})
+	}
 }
